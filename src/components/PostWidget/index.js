1baf9c72bf49c6f5e3cc4818931ecb1f5d4c4d82
Diego Rocha <dkxever@gmail.com>
2022-05-25 01:04:22 -0300

    Structuring and organizing our PostWidget and tailwindcss

--- src/components/PostWidget/index.js
+++ src/components/PostWidget/index.js
@@ -21,8 +21,32 @@ export const PostWidget = ({ slug, categories }) => {
   },[ slug, categories ]);
 
   return (
-    <div>
-      
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl font-semibold border-b pb-4 mb-8">
+        { slug ? "Postagens Relacionadas" : "Postagens Recentes" }
+      </h3>
+      {relatedPosts.map((post, index) => (
+        <div key={index} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <Image
+              unoptimized
+              src={post.featuredImage.url}
+              alt={post.title}
+              width="60px"
+              height="60px"
+              className="align-middle rounded-full"
+            />
+          </div>
+          <div className="flex-grow ml-2">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format('DD MMM YYYY')}
+            </p>
+            <Link key={index} href={`/post/${post.slug}`} className="text-md">
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   )
 
