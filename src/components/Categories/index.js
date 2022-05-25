cbb65fb7b798820fe46358645a4d4e55d0895f7f
Diego Rocha <dkxever@gmail.com>
2022-05-25 01:07:36 -0300

    Structuring and organizing our Categories and tailwindcss

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -13,9 +13,17 @@ export const Categories = () => {
   },[]);
   
   return (
-    <div>
-      
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl font-semibold border-b pb-4 mb-8">
+        Categorias
+      </h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className={`cursor-pointer block ${ index === category.length - 1 ? 'border-b-0' : 'border-b' } pb-3 mb-3`}>{category.name}</span>
+        </Link>
+      ))}
     </div>
   )
+  
 }
 
