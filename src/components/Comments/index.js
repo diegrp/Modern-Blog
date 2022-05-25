8bc794d9b459ebd4771e314fdeddcd3bf37f9e38
Diego Rocha <dkxever@gmail.com>
2022-05-25 01:14:59 -0300

    Structuring and organizing our Comments and tailwindcss

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -17,8 +17,28 @@ export const Comments = ({ slug }) => {
   /* Retorna os comentários de um derterminado post, se tiver ao menos um comentário*/
 
   return (
-    <div>
-      
-    </div>
+    <>
+      {comments.length > 0 && (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            {comments.length}
+            {' '}
+            Comentários
+          </h3>
+            {comments.map((comment, index) => (
+              <div key={index} className="border-b border-gray-100 pb-4 mb-4">
+                <p className="mb-4">
+                  <span className="font-semibold">{comment.name}</span>
+                  {' '}
+                  em
+                  {' '}
+                  {moment(comment.createdAt).format('DD MMM YYYY')}
+                </p>
+                <p className="whitespace-pre-line w-full text-gray-600">{parser(comment.comment)}</p>
+              </div>
+            ))}
+        </div>
+      )}
+    </>
   )
 }
