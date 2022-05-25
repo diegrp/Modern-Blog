4621a4df17bf9f15e2a71400ecc340fb7d3357b4
Diego Rocha <dkxever@gmail.com>
2022-05-25 01:11:38 -0300

    Structuring and organizing our PostDetail and tailwindcss

--- src/components/PostDetail/index.js
+++ src/components/PostDetail/index.js
@@ -50,8 +50,43 @@ export const PostDetail = ({ post }) => {
   };
 
   return (
-    <div>
-      
+    <div className="bg-white rounded-lg shadow-lg lg:p-8 pb-12 mb-8">
+      <Head>
+        <title>Modern Blog - {post.title}</title>
+        <link rel="ico" href="./favicon.icon" />
+      </Head>
+      <div className="relative overflow-hidden relative shadow-md mb-6">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="object-top object-cover w-full h-full shadow-lg rounded-t-lg lg:rounded-lg"
+        />
+      </div>
+      <div className="px-8 lg:px-0">
+        <div className="flex items-center justify-center mb-8 w-full">
+          <div className="flex items-center jusitfy-center mb-4 mr-8 lg:w-auto lg:mb-0">
+            <Image
+              src={post.author.photo.url}
+              alt={post.author.name}
+              width="30px"
+              height="30px"
+              className="align-middle rounded-full"
+            />
+            <p className="inline align-middle text-gray-700 font-medium text-lg ml-2">{post.author.name}</p>
+          </div>
+          <div className="font-medium text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+            <span className="align-middle">{moment(post.createdAt).format('DD MMM YYYY')}</span>
+          </div>
+        </div>
+        <h1 className="text-3xl font-semibold mb-8">{post.title}</h1>
+        {post.content.raw.children.map(( typeObj, index ) => {
+          const children = typeObj.children.map(( item, itemIndex ) => getContentFragment( itemIndex, item.text, item ));
+          return getContentFragment(index, children, typeObj, typeObj.type);
+        })}
+      </div>
     </div>
   )
 
