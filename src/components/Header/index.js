8e77e5fd352e99a097af5f14c8090d321972f051
Diego Rocha <dkxever@gmail.com>
2022-05-25 00:57:54 -0300

    Structuring and organizing our Header and tailwindcss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,8 +15,21 @@ export const Header = () => {
   },[]);
 
   return (
-    <div>
-      
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left text-center">
+          <Link href="/">
+            <span className="cursor-pointer text-4xl text-white font-bold">Modern Blog - GraphCMS</span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 ml-4 align-middle text-white font-semibold cursor-pointer">{category.name}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
